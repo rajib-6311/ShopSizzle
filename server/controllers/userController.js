fe7b6cfcfd411f93680d2ec3d6fed47cd3d05857
rajib hossen <mhossen6311@gmail.com>
2025-08-05 23:26:08 +0600

    update login thorugh backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,33 +1,45 @@
 import validator from 'validator';
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import userModel from "../models/userModels.js";
 
-const userRegister = async (req, res) =>{
-    try{
-        const {name,email,password} = req.body;
-       
+const createToken = (user) => {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+        name: user.name
+    },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+    )
+}
+
+const userRegister = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
         // Request body verification
-        if(!name){
+        if (!name) {
             return res.json({
-                success:false,
+                success: false,
                 message: 'Name is required'
             })
         }
-        if(!email){
+        if (!email) {
             return res.json({
-                success:false,
+                success: false,
                 message: 'Email is required'
             })
         }
-        if(!password){
+        if (!password) {
             return res.json({
-                success:false,
+                success: false,
                 message: 'Password is required'
             })
         }
 
         // email validation
-        if(!validator.isEmail(email)){
+        if (!validator.isEmail(email)) {
             return res.json({
                 success: false,
                 message: 'Please enter a valid email'
@@ -35,13 +47,13 @@ const userRegister = async (req, res) =>{
         }
 
         // Cheek user status 
-        const existingUser = await userModel.findOne({email});
-        if(existingUser){
-          return res.json({success:true, message: 'User already exist'})
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.json({ success: true, message: 'User already exist' })
         }
 
         // password validation
-        if(password.length < 8){
+        if (password.length < 8) {
             return res.json({
                 success: true,
                 message: 'Give password more than 8 charter'
@@ -55,7 +67,7 @@ const userRegister = async (req, res) =>{
         const newUser = new userModel({
             name,
             email,
-            password : encryptedPassword,
+            password: encryptedPassword,
         })
 
         // save the user in database
@@ -65,21 +77,71 @@ const userRegister = async (req, res) =>{
             success: true,
             message: 'Users register successfully'
         })
-       
-    }catch(error){
+
+    } catch (error) {
         console.log('User register ERROR', error)
-        res.json({success:true, message:error?.error})
+        res.json({ success: true, message: error?.error })
     }
 }
-const userLogin = async (req, res) =>{}
-const adminLogin = async (req, res) =>{}
-const removeUser = async (req, res) =>{}
-const updateUser = async (req, res) =>{}
-const getUser = async (req, res) =>{
+const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.json({
+                success: false,
+                message: 'Email is required'
+            })
+        }
+
+        // If user exist 
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "User doesn't exist"
+            })
+        }
+
+        if (!password) {
+            return res.json({
+                success: false,
+                message: 'Password is required'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if (isMatch) {
+            const token = createToken(user)
+            res.json({
+                success: true,
+                token,
+                message: 'Users logged in successfully'
+            })
+
+        }
+        else {
+            return res.json({
+                success: false,
+                message: 'Invalid credentials, try again'
+            })
+        }
+
+
+    } catch (error) {
+        console.log('User login ERROR', error)
+        res.json({ success: true, message: error?.error })
+    }
+}
+const adminLogin = async (req, res) => { }
+const removeUser = async (req, res) => { }
+const updateUser = async (req, res) => { }
+const getUser = async (req, res) => {
     res.send('find user')
 }
 
-export{
+export {
     userLogin,
     userRegister,
     adminLogin,
--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json())
+
 // MongoDB connection 
 dbConnection()
 
